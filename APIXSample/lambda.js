f3135f1de2d0db25b365bea0aa09d4b8c22f4346
sigmaidetest <cehib@balanc3r.com>
2018-11-12 16:19:43 +0530

    Added token API call

--- APIXSample/lambda.js
+++ APIXSample/lambda.js
@@ -1,5 +1,22 @@
-exports.handler = function(event, context, callback) {
+let Swagger = require('swagger-client');
+
+exports.handler = function (event, context, callback) {
+
+    Swagger.http({
+        url: `https://services.apixplatform.com/api-sandbox/application/token`,
+        method: 'post',
+        query: {},
+        headers: { "Accept": "*/*", "Content-Type": "application/json" },
+        body: {
+            "id": 123
+        }
+    }).then((response) => {
+        console.log("Received response", response);
+        callback(null, response);
+    }).catch((err) => {
+        console.log("Error occurred", err);
+        callback(err);
+    });
+
     
-    console.log("Hello World");
-    callback(null, {"message": "Hello World"});
 }
